8a6125753260ce2b831bd1620f8c48e6422836b6
jonnicholson94 <jon.nicholson94@googlemail.com>
2023-04-03 19:05:49 +0100

    Updated images on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,32 +30,32 @@ const HomePage = () => {
           icon={faPlus}
           heading="Link all of your tasks"
           content="Give it a name, write a description, select its category and more. Track all of your tasks with Kanby, with less of the stress."
-          img="/general-info.png" />
+          img="/1.png" />
         <InfoSection
           iconBackground='var(--blue)'
           icon={faCalendar}
           heading="See everything on your dashboard"
           content="Track the status of all of your tasks on your dashboard. See where everything's at, and plan your days accordingly."
-          img="/board.png" />
+          img="/2.png" />
         <InfoSection
           iconBackground='var(--green)'
           icon={faList}
           heading="Choose a category"
           content="Choose from a variety of different categories so each task is easy to identify and separate. Manage your day more effectively by breaking everything down into the different parts of your life."
-          img="/categories.png" />
+          img="/3.png" />
         <InfoSection
-          iconBackground='var(--salmon'
+          iconBackground='var(--light-pink'
           icon={faComments}
           heading="Add comments"
           content="Post comments on each of your tasks allowing you to quickly and easily see the latest status on everything, at a glance. No more vague tasks."
-          img="/comments.png" />
+          img="/4.png" />
         <InfoSection
           iconBackground='var(--grey)'
           color="white"
           icon={faCheck}
           heading="Add sub tasks"
           content="Got a bigger task? No problem! Add sub tasks to your big task so you know exactly what needs doing."
-          img="/sub-tasks.png" /> 
+          img="/5.png" /> 
         <Footer />
       </>
     )
